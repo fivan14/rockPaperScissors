54916304d6d7f687c2ca8fc4d7ecdba9650284d4
fivan14 <ivucetic7792@gmail.com>
2023-02-15 22:02:58 +0100

    Add 5 rounds game logic

--- script.js
+++ script.js
@@ -35,11 +35,39 @@ console.log(computersChoice(symbols))
 const userPlay = (arr) => {
     let userInput = prompt('Enter your symbol:')
     if(arr.includes(userInput.toLowerCase())) {
-        return userInput
+        return userInput.toLowerCase()
     } else {
         alert('Did you entered rock, paper or scissors?')
         return userPlay(arr)
     }
 }
 
-userPlay(symbols)
+// userPlay(symbols)
+
+// creating for loop, playing 5 rounds per game
+let totalUser = 0;
+let totalAI = 0;
+
+for (let i = 0; i < 5;) {
+    
+    let user = userPlay(symbols)
+    let computer = computersChoice(symbols)
+    if(outcomes[user][computer] > 0) {
+        console.log(`You won! ${user} beats ${computer}`)
+        i++
+        totalUser++
+    } else if (outcomes[user][computer]) {
+        console.log(`You lost! ${computer} beats ${user}`)
+        i++
+        totalAI++
+    } else {
+        console.log(`It's a draw! You both chose ${user}`)
+    }
+
+}
+
+if(totalUser > totalAI) {
+    console.log(`User won-score is: ${totalUser}:${totalAI}`)
+} else {
+    console.log(`AI won-score is: ${totalUser}:${totalAI}`)
+}
