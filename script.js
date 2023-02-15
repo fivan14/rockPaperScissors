25f2fede7b4add483db8e2fced99256a99001ec0
fivan14 <ivucetic7792@gmail.com>
2023-02-15 19:29:05 +0100

    Add table with possible outcomes

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // add a table with possible outcomes for every game
 
+const outcomes = {
+    rock: {
+        rock: 0, 
+        paper: -1,
+        scissors: 1,
+    },
+
+    paper: {
+        rock: 1,
+        paper: 0,
+        scissors: -1,
+    },
+
+    scissors: {
+        rock: -1,
+        paper: 1,
+        scissors: 0,
+    }
+}
